807f1188ef3a380b366f89dca9fc84802f3e45f5
AverageMalhik <muhdjibran@hotmail.com>
2021-07-18 09:06:06 +0530

    added page transition

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,26 @@ void main() =>
     runApp(MaterialApp(
       initialRoute: '/',
       //implement onGenerateRoute for page transitions
-      // onGenerateRoute: (settings) {
-      //   switch (settings.name) {
-      //     case '/': return MaterialPageRoute(builder: (_) => CarouselMain());
-      //     case '/result':
-      //   }
-      // },
-      routes: {
-        '/': (context) => CarouselMain(),
-        '/result': (context) => Result()
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/': return MaterialPageRoute(builder: (_) => CarouselMain());
+          case '/result':
+            return PageRouteBuilder(
+                settings: settings, // Pass this to make popUntil(), pushNamedAndRemoveUntil(), works
+                pageBuilder: (context,animation,secondaryAnimation) => Result(),
+                transitionsBuilder: (context,animation,secondaryAnimation,child) {
+                  var begin = Offset(0.0, 2.0);
+                  var end = Offset.zero;
+                  var curve = Curves.ease;
+
+                  var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                  return SlideTransition(
+                    position: animation.drive(tween),
+                    child: child,
+                  );
+                },
+            );
+        }
       },
     ));
 
