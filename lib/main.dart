2a65703da5e2bbc8838288c9bfeeb90a65690f97
AverageMalhik <muhdjibran@hotmail.com>
2021-07-18 08:51:12 +0530

    add comment

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(MaterialApp(
       initialRoute: '/',
+      //implement onGenerateRoute for page transitions
       // onGenerateRoute: (settings) {
       //   switch (settings.name) {
       //     case '/': return MaterialPageRoute(builder: (_) => CarouselMain());
